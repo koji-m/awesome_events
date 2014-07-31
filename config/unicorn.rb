3fa91e3442bb595983941e2520cb6046b7664537
Koji Matsumoto <otomustam.ijok@gmail.com>
2014-07-31 16:33:34 +0900

    furthermore mod unicorn.rb for debug

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -23,17 +23,23 @@ print "<<< DEBUG >>> OK after setting stdout_path AND stderr_path"
 # before_fork、after_forkでは、Unicornのプロセスがフォークする前後の
 # 挙動を指定できる。以下のおまじないの詳細はドキュメントを参考のこと。
 before_fork do |server, worker|
+print "<<< DEBUG >>> OK next defined? IN before_fork"
   defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
-
+print "<<< DEBUG >>> OK next old_pid = IN before_fork"
   old_pid = "#{ server.config[:pid] }.oldbin"
+print "<<< DEBUG >>> OK next unless old_pid IN before_fork"
   unlsess old_pid == server.pid do
     begin
+print "<<< DEBUG >>> OK next 1 IN before_fork"
       Process.kill :QUIT, File.read(old_pid).to_i
+print "<<< DEBUG >>> OK next 2 IN before_fork"
     rescue Errno::ENOENT, Errno::ESRCH
     end
   end
 end
 
 after_fork do |server, worker|
+print "<<< DEBUG >>> OK next 3 IN before_fork"
   defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+print "<<< DEBUG >>> OK next 4 IN before_fork"
 end
